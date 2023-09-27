f1caf4b977f313fde3e44cb954d257181a420e48
Hoàng Quý <78021023+quysterben@users.noreply.github.com>
2023-09-28 01:24:45 +0700

    bqd: test cases

--- handleStudent.test.js
+++ handleStudent.test.js
@@ -54,3 +54,41 @@ test("PHTD - Should be return Weak", () => {
    expect(handleStudent.getStudentType(4, 5, 6, 7, handleStudent.ConductType.Rather))
       .toBe(handleStudent.StudentType.Weak)
 })
+
+test("BQD - Should be return Good", () => {
+   expect(handleStudent.getStudentType(8, 8, 8, 8, handleStudent.ConductType.Good))
+      .toBe(handleStudent.StudentType.Good)  
+})
+
+test("BQD - Should be return Rather", () => {
+   expect(handleStudent.getStudentType(8, 8, 8, 9, handleStudent.ConductType.Rather))
+      .toBe(handleStudent.StudentType.Rather)  
+   expect(handleStudent.getStudentType(8, 7, 8, 9, handleStudent.ConductType.Good))
+      .toBe(handleStudent.StudentType.Rather)
+   expect(handleStudent.getStudentType(8, 7, 8, 9, handleStudent.ConductType.Rather))
+      .toBe(handleStudent.StudentType.Rather)
+   expect(handleStudent.getStudentType(7.5, 7.5, 7.5, 7.5, handleStudent.ConductType.Rather))
+      .toBe(handleStudent.StudentType.Rather)
+})
+
+test("BQD - Should be return Medium", () => {
+   expect(handleStudent.getStudentType(8, 8, 8, 9, handleStudent.ConductType.Medium))
+      .toBe(handleStudent.StudentType.Medium)  
+   expect(handleStudent.getStudentType(8, 7, 8, 9, handleStudent.ConductType.Medium))
+      .toBe(handleStudent.StudentType.Medium)
+   expect(handleStudent.getStudentType(7.5, 7.5, 7.5, 7.5, handleStudent.ConductType.Medium))
+      .toBe(handleStudent.StudentType.Medium)
+   expect(handleStudent.getStudentType(6.5, 6.5, 6.5, 6.5, handleStudent.ConductType.Medium))
+      .toBe(handleStudent.StudentType.Medium)
+})
+
+test("BQD - Should be return Weak", () => {
+   expect(handleStudent.getStudentType(8, 8, 8, 9, handleStudent.ConductType.Weak))
+      .toBe(handleStudent.StudentType.Weak)  
+   expect(handleStudent.getStudentType(8, 7, 8, 9, handleStudent.ConductType.Weak))
+      .toBe(handleStudent.StudentType.Weak)
+   expect(handleStudent.getStudentType(7.5, 7.5, 7.5, 7.5, handleStudent.ConductType.Weak))
+      .toBe(handleStudent.StudentType.Weak)
+   expect(handleStudent.getStudentType(5.5, 5.5, 5.5, 5.5, handleStudent.ConductType.Good))
+      .toBe(handleStudent.StudentType.Weak)
+})
